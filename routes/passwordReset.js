5ab28c5742018cf95c64a034aa1bffc85e8fe020
Victor David <vdavid@proteccion.com.co>
2022-03-24 18:42:39 -0500

    Recuperar contraseña

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -3,6 +3,7 @@ const Token = require("../models/token");
 const sendEmail = require("../utils/sendEmail");
 const crypto = require("crypto");
 const Joi = require("joi");
+const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
 
@@ -36,6 +37,7 @@ router.post("/", async (req, res) => {
 
 router.post("/:userId/:token", async (req, res) => {
     try {
+        const salt = await bcrypt.genSalt(10);
         const schema = Joi.object({ password: Joi.string().required() });
         const { error } = schema.validate(req.body);
         if (error) return res.status(400).send(error.details[0].message);
@@ -49,11 +51,11 @@ router.post("/:userId/:token", async (req, res) => {
         });
         if (!token) return res.status(400).send("Invalid link or expired");
 
-        user.password = req.body.password;
+        user.password = await bcrypt.hash(req.body.password, salt);
         await user.save();
         await token.delete();
 
-        res.status(200).send("password reset sucessfully.");
+        res.status(200).json();
     } catch (error) {
         res.status(500).send("An error occured");
         console.log(error);
